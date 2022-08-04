91415b8fdf682a21f05cce4a549e4ea4bc06b764
QIQIQI1112 <47882151+QIQIQI1112@users.noreply.github.com>
2022-08-04 10:51:27 -0500

    Add files via upload

---
+++ example/mc.py
@@ -0,0 +1,75 @@
+from ase.spacegroup import crystal
+from readinginput import *
+from randommove import *
+from symdata import *
+
+root_path = os.getcwd()
+trail_path = root_path +'/trail'
+
+# output file
+path_output = root_path +'/output'
+isfile1 = os.path.isfile(path_output)
+isfile = str(isfile1)
+if isfile == 'False':
+    os.mkdir('output')
+
+# log
+log = WriteLog(path_output)
+script_count = log.write_script_count()
+big_loop = log.write_big_loop(script_count, internal_circulation)
+
+# read POSCAR
+poscar = ReadPOSCAR('POSCAR', trail_path, root_path)
+cell_poscar = poscar.lattice
+
+# get rigid and single atom array
+total_atom = rigid_atom + single_Atom
+system_initial = read_system_initial(trail_path, total_atom)
+rigid_array = system_initial[0:rigid_atom, :]
+single_array = system_initial[rigid_atom:total_atom, :]
+
+# generate new system
+single_atom_symmetry_len = len(single_atom_symmetry)
+wyckoff_position_single = []
+for i in range(single_atom_symmetry_len):
+    wyckoff_position_single.append(sg[f'sg_{sym_no}'][single_atom_symmetry[i]][2])
+compound = [*compound_rigid, *(compound_single_atom * single_Atom)]
+
+# wyckoff position
+wyckoff_position_rigid = sg[f'sg_{sym_no}'][f's{wyckoff_rigid}'][2]
+
+# current temp
+temp = temp_algorithm(temp_update, temp_star, big_loop)
+
+close_dis_check = 'no'
+print('looking for new atom positions...')
+while close_dis_check != 'yes':
+    rigid_after_move1 = AtomRandomMove(rigid_array).symmetry_restricted('rigid', step_update, temp, wyckoff_position_rigid, rigid_type)
+    # image select
+    rigid_after_move = rigid_after_move1.copy()
+    for i in range(1, rigid_atom):
+        image_select = AtomImage(rigid_after_move1, cell_poscar ).close_image_position(rigid_after_move1[0])
+        rigid_after_move[i] = image_select
+    
+    # rotation
+    rigid_after_rotation = rigid_after_move.copy()
+    # single atom move
+    single_after_move = AtomRandomMove(single_array).symmetry_restricted('single', step_update, temp, wyckoff_position_single)
+
+    system_after_move = np.concatenate((rigid_after_rotation, single_after_move))
+    # CtoD
+    system_after_move_dir = direct_cartesian_transform(system_after_move, cell_poscar, 'CtoD')
+    system_after_move_tuple = []
+    for k in range(total_atom):
+        system_after_move_tuple.append(tuple(system_after_move_dir[k]))
+    # transform
+    system_transform = crystal(compound, system_after_move_tuple, spacegroup=sym_no, cell=cell_poscar)
+    close_dis_check = dis_check(system_transform.positions, cell_poscar, dis_limit)
+
+    # write poscar and system_initial
+    os.chdir(trail_path)
+    write_poscar(system_transform.positions, compound, rigid_muti, ratio, cell_poscar)
+    write_system_initial(system_after_move)
+    os.chdir(root_path)
+
+    print(f'current step : {step_update}')
---
+++ example/metropolis.py
@@ -0,0 +1,123 @@
+from functions import *
+from readinginput import *
+
+root_path = os.getcwd()
+trail_path = root_path +'/trail'
+output_path = root_path +'/output'
+log_path = root_path +'/log'
+os.chdir(trail_path)
+
+# get energy from outcar and outcar_ref
+ref_energy = read_energy('OUTCAR_ref')
+energy = read_energy('OUTCAR')
+print('reference energy : ' + str(ref_energy))
+print('energy : ' + str(energy))
+
+os.chdir(output_path)
+with open('big_loop', 'r') as scrip_read:
+    line_script = scrip_read.readlines()
+    big_loop = float(line_script[0].split()[0])
+
+# metroplis
+current_temp = temp_algorithm(temp_update, temp_star, big_loop)
+accept = metropolis_group_single('default', energy, ref_energy, current_temp)
+print('current temp : {}'.format(current_temp))
+
+os.chdir(trail_path)
+if accept == 1:
+    shutil.copy('POSCAR', 'POSCAR_ref')
+    shutil.copy('OUTCAR', 'OUTCAR_ref')
+    shutil.copy('system_initial', 'system_initial_ref')
+    print('accept : yes')
+else:
+    print('accept : no')
+
+# calculate accept rate
+os.chdir(output_path)
+isfile1 = os.path.isfile(log_path)
+isfile = str(isfile1)
+if isfile == 'False':
+    with open('log', 'a') as log_count:
+        for i in range(101):
+            log_count.write('0' + '\n')
+        log_count.write('1' + '\n')
+
+with open('log', 'r') as log_read:
+    lines = log_read.readlines()
+
+loop_current = int(lines[100])
+current_step = int(lines[101])
+
+with open('log', 'w') as log_edit:
+    for i, line in enumerate(lines):
+        if accept == 1 and i == loop_current:
+            log_edit.writelines('1\n')
+        elif accept == 0 and i == loop_current:
+            log_edit.writelines('0\n')
+        else:
+            log_edit.writelines(line)
+
+with open('log', 'r') as log_read_2:
+    lines_2 = log_read_2.readlines()
+
+zero_count = 0
+one_count = 0
+print(f'current step {current_step}')
+if current_step < 100:
+    for i in range(current_step):
+        if str(lines_2[i]) == '0\n':
+            zero_count += 1
+        elif str(lines_2[i]) == '1\n':
+            one_count += 1
+else:
+    for i in range(100):
+        if str(lines_2[i]) == '0\n':
+            zero_count += 1
+        elif str(lines_2[i]) == '1\n':
+            one_count += 1
+
+accept_rate = round(one_count / (zero_count + one_count), 5)
+
+print(f'current acceptance rate : {accept_rate * 100}%')
+
+if loop_current < 99:
+    loop_current += 1
+else:
+    loop_current = 0
+
+current_step += 1
+
+# write log
+with open('log', 'w') as log_edit_2:
+    for i, line in enumerate(lines_2):
+        if i == 100:
+            log_edit_2.writelines(str(loop_current) + '\n')
+        elif i == 101:
+            log_edit_2.writelines(str(current_step) + '\n')
+        else:
+            log_edit_2.writelines(line)
+
+# write energy
+# if accept == 1:
+with open('energy', 'a') as en:
+    en.write(f'{current_step}  {energy}' + '\n')
+
+# energy accept
+if accept == 1:
+    with open('energy_acc', 'a') as en_acc:
+        en_acc.write(f'{current_step}  {energy}' + '\n')
+elif accept == 0:
+    with open('energy_rej', 'a') as en_rej:
+        en_rej.write(f'{current_step}  {energy}' + '\n')
+
+# debug
+if debug == 1:
+    debug_path = output_path + '/debug'
+    isdir1 = os.path.isdir(debug_path)
+    isdir = str(isdir1)
+    if isdir == 'False':
+        os.mkdir('debug')
+    os.chdir(trail_path)
+    shutil.copy('POSCAR', output_path + f'/debug/POSCAR_{int(current_step)}')
+
+os.chdir(root_path)
